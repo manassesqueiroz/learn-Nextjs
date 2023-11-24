748e2885a04bd835d1cd538475b170cc11fb0485
Manassés Queiroz <manassescruzgt@gmail.com>
2023-11-24 19:17:02 -0300

    improve accessibility end validators

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -30,7 +30,9 @@ export default async function InvoicesTable({
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
-                        height={28} alt={''}                      />
+                        height={28}
+                        alt={`${invoice.name}'s profile picture'`}                      
+                        />
                       <p>{invoice.name}</p>
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>
