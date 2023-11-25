ca0b470898df77d668e6ea42a6acefe711c487a6
Manassés Queiroz <manassescruzgt@gmail.com>
2023-11-24 21:49:55 -0300

    created auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+
 
 const FormSchema = z.object({
   id: z.string(),
@@ -111,4 +113,19 @@ export async function deleteInvoice(id: string) {
   }
   
   revalidatePath('/dashboard/invoices');
+} 
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
+  }
 }
