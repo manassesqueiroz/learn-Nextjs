d9b3e7d5dce987fb8c9e2f873436fd503c04d3cc
Manassés Queiroz <manassescruzgt@gmail.com>
2023-12-21 18:06:31 -0300

    update in pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
-export default function Customers() {
-    return <p>Dashboard Page Cuatomers</p>;
-}   
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers, } from '@/app/lib/data';
+
+export default async function Page() {
+    const Customers = await fetchFilteredCustomers('')
+    return (
+    <div className="w-full">
+        <div className="flex w-full items-center justify-between">
+            <CustomersTable customers={Customers} />
+        </div>
+    </div>
+    )
+}
